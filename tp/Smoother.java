12a212e28b8d1a9fe07ea56a6583dc5c86775505
Mathieu Mattei <mattei.mathieu2b1@gmail.com>
2020-11-05 21:02:15 +0100

    first commit

---
+++ tp/Smoother.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp;
+
+/**
+ *
+ * @author matte
+ */
+import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_imgproc.*;
+import static org.bytedeco.opencv.global.opencv_core.*;
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
+
+public class Smoother {
+    public static void smooth(String filename) {
+        Mat image = imread(filename);
+        if (image != null) {
+            GaussianBlur(image, image, new Size(3, 3), 0);
+            imwrite(filename, image);
+        }
+    }
+}
